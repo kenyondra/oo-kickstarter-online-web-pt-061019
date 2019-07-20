9f13336f62b17937e5f8bfef072876be6daa5cfc
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-07-20 04:47:29 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,4 +8,4 @@ class Project
   
   def add_backer(backer)
     @backers << backer 
-    backer.back_project(self) unless backer.backed_projects.include 
+    backer.back_project(self) unless backer.backed_projects.include?(self) 
